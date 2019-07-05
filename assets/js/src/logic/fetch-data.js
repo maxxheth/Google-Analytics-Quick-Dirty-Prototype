ae6d9e14e2804a496cd727e43a049dfc43471888
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-05 14:03:48 -0500

    Wrapped axios POST request in an exportable func

--- assets/js/src/logic/fetch-data.js
+++ assets/js/src/logic/fetch-data.js
@@ -2,23 +2,27 @@ import axios from 'axios';
 
 const googleAPIPath = '../../../../fetchgapidata.php';
 
-axios.post(googleAPIPath, {
-
-    dataParams: {
-        startDate: "30daysAgo",
-        endDate: "today",
-    },
-
-    GAPIParams: {
-
-        expression: "ga:uniquePageViews",
-        alias: "uniquePageViews"
-
-    }
+export const fetchData = () => {
     
+    axios.post(googleAPIPath, {
 
-})
+        dataParams: {
+            startDate: "30daysAgo",
+            endDate: "today",
+        },
 
-.then(response => console.log(response))
+        GAPIParams: {
 
-.catch(error => console.log(error));
+            expression: "ga:uniquePageViews",
+            alias: "uniquePageViews"
+
+        }
+        
+
+    })
+
+    .then(response => console.log(response))
+
+    .catch(error => console.log(error));
+
+};
