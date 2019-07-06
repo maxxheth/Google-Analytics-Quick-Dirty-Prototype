ea667b32f01e6617292ca7a90e15000d9d0912ab
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-06 14:56:25 -0500

    Adjusted paths due to path resolution error

--- helper_funcs/helperFuncsLoader.php
+++ helper_funcs/helperFuncsLoader.php
@@ -3,9 +3,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require(__DIR__ . '/helper_funcs/pretty_print.php');
-require(__DIR__ . '/helper_funcs/htmltrim.php');
-require(__DIR__ . '/helper_funcs/filterNestedArrayArgs.php');
-require(__DIR__ . '/helper_funcs/massArrayMerge.php');
-require(__DIR__ . '/helper_funcs/unpackArgs.php');
-require(__DIR__ . '/helper_funcs/generateReportArray.php');
+require(__DIR__ . '\pretty_print.php');
+require(__DIR__ . '\htmltrim.php');
+require(__DIR__ . '\filterNestedArrayArgs.php');
+require(__DIR__ . '\massArrayMerge.php');
+require(__DIR__ . '\unpackArgs.php');
+require(__DIR__ . '\generateReportArray.php');
