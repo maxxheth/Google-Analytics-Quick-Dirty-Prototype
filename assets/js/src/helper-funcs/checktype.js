7be25057f23ca9a6837ff9b551384d574a94a4d2
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-07 00:37:10 -0500

    Set up helper func for type checking

---
+++ assets/js/src/helper-funcs/checktype.js
@@ -0,0 +1,7 @@
+export const checkType = typeTarget => {
+
+    const objectType = Object.prototype.toString.call(typeTarget);
+
+    return objectType.slice(8, objectType.length - 1).toLowerCase();
+
+};
