5c133cfa15eb7cc10a0a70ced0eefd8e31fb994f
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-07 16:47:24 -0500

    Added comments

--- assets/js/src/logic/pipe-gapi-data.js
+++ assets/js/src/logic/pipe-gapi-data.js
@@ -1,13 +1,32 @@
+/**
+ * The idea is to take a raw object (or several) with two properties,
+ * pipe them through one of three functions that'll assign a unique property name
+ * to each pair, and then combine all pairs into an object by mapping multiple pairs
+ * with the same property name to an array under said property name.
+ * 
+ * The object will then be sent to a PHP script via an AJAX POST request that'll 
+ * then unpack the data in each request object, 
+ * reformat it for proper consumption by the Google Analytics Reporting API, 
+ * pipe it to the Google Analytics Reporting API, and return a object containing 
+ * the requested metric categories and associated data.  
+ * 
+ * 
+ */
+
 import { checkType } from '../../src/helper-funcs/checktype';
 
 /**
  * @function setMetrics
  *  
- * 1. This function takes two arguments and returns an object with the property name "metricArgs".
- * The goal of this function is to provide a way to   
+ * This function takes two arguments and returns an object with the property name "metricArgs".
+ * The goal of this function is to return an object with a parent property that contains two child properties.
  * 
- * @param { string } expression || A metric such as "ga:sessions" is inserted as an argument.
- * @param { string } alias || A shorthand way to refer to the expression.
+ * The first property is a expression that corresponds with a Google Analytics metric property (e.g, "ga:sessions")
+ * 
+ * The second property is an alias of that expression. Thus, "ga:session" can simply be expressed as "session" per the alias property. 
+ * 
+ * @param { string } expression | A metric such as "ga:sessions" is inserted as an argument.
+ * @param { string } alias | A shorthand way to refer to the expression.
  * 
  *   
  */
@@ -18,22 +37,15 @@ export const setDates = startDate => endDate => ( { dateArgs: [ { startDate, end
 
 export const setDimensions = name => histoGram => ( { dimensionArgs: [ { name, histoGram } ] });
 
-
 /**
  * 
  * @function pipeDataPropPairs
  * 
- * @param dataPropPairs | This makes use of the rest syntax to allow as many
+ * @param { callback }
+ * 
+ * @param { dataPropPairs } | This makes use of the rest syntax to allow as many
  * data objects to be inserted as possible.
  * 
- * The idea is to take a raw object (or several) with two properties,
- * pipe them through one of three functions that'll assign a unique property name
- * to each pair, and then combine all pairs into an object by mapping multiple pairs
- * with the same property name to an array under said property name.
- * 
- * The object will then be piped to the PHP script that'll 
- * then unpack the data under each property name, pipe it to the Google Analytics API services, and
- * return a object containing the requested metric categories and data.
  * 
  */
 
