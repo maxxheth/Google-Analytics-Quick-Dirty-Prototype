dd4fa3da2b962c571f69b42ce5793f20631e37f6
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-07 23:34:34 -0500

    Set up GAPI params for option components

---
+++ assets/js/src/data/gapi-options.js
@@ -0,0 +1,41 @@
+export const GAOptions = [
+
+        {
+            value: "ga:users",
+            text: "Users"
+        },
+        {
+            value: "ga:newUsers",
+            text: "New Users"
+        },
+        {
+            value: "ga:bounces",
+            text: "Bounces"
+        },
+        {
+            value: "ga:bounceRate",
+            text: "Bounce Rate"
+        },
+        {
+            value: "ga:sessions",
+            text: "Sessions"
+        },
+        {
+            value: "ga:sessionDuration",
+            text: "Session Duration"
+        },
+        {
+            value: "ga:avgSessionDuration",
+            text: "Average Session Duration"
+        },
+        {
+            value: "ga:hits",
+            text: "Hits"
+        },
+        
+        {
+            value: "ga:organicSearches",
+            text: "Organic Searches"
+        },
+    
+];
