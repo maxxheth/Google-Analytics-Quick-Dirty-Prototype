0b15bd06a5b3aee5e8b0ca7240d738cfd4ccfaa0
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-09 03:24:28 -0500

    Created date range data objects in data folder

---
+++ assets/js/src/data/date-ranges.js
@@ -0,0 +1,34 @@
+export const dateRangeObject = {
+
+    today: [0, "days"],
+    yesterday: [1, "days"],
+    seven_days: [7, "days"],
+    fourteen_days: [14, "days"],
+    twentyeight_days: [28, "days"]
+
+};
+
+export const dateRangeOptions = [
+
+    {
+        value: "today",
+        text: "Today"
+    },
+    {
+        value: "yesterday",
+        text: "Since yesterday"
+    },
+    {
+        value: "seven_days",
+        text: "1 week ago"
+    },
+    {
+        value: "fourteen_days",
+        text: "2 weeks ago"
+    },
+    {
+        value: "twentyeight_days",
+        text: "4 weeks ago"
+    }
+
+];
