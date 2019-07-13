2173f8589b308c66c80ff5009b911e2fe02db645
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-13 13:45:57 -0500

    Refactored code for more effective array mapping

--- assets/js/src/logic/unpack-gapi-payloads.js
+++ assets/js/src/logic/unpack-gapi-payloads.js
@@ -1,8 +1,9 @@
+
 import { createGAPIRequestObject } from './pipe-gapi-data';
 
 import { fetchData } from './fetch-data';
 
-export const unpackGAPIPayloads = payload => {
+export const unpackGAPIPayloads = payload => { 
 
     const { metricsPayload, dateRangePayload } = payload;
 
@@ -10,15 +11,15 @@ export const unpackGAPIPayloads = payload => {
 
     const metricsPropObjects = [];
 
-    for (let x = 0; x < liftedDateRangePropObjects.dateArgs.length; x++) metricsPropObjects.push(x);
+    for ( let x = 0; x < liftedDateRangePropObjects.dateArgs.length; x++ ) metricsPropObjects.push(x);
 
-    metricsPropObjects.fill(metricsPayload, 0, liftedDateRangePropObjects.dateArgs.length);
+        metricsPropObjects.fill( metricsPayload, 0, liftedDateRangePropObjects.dateArgs.length );
 
-    const liftedMetricsPropObjects = createGAPIRequestObject(metricsPropObjects);
+        const liftedMetricsPropObjects = createGAPIRequestObject( metricsPropObjects );
 
-    if (liftedMetricsPropObjects.metricArgs.length === liftedDateRangePropObjects.dateArgs.length) {
+    if ( liftedMetricsPropObjects.metricArgs.length === liftedDateRangePropObjects.dateArgs.length ) {
 
-        fetchData(liftedMetricsPropObjects, liftedDateRangePropObjects);
+        fetchData( liftedMetricsPropObjects, liftedDateRangePropObjects );
 
     }
 
