604fef5b950bcfa5829a7ce0fd10ad15dc73aaa0
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-13 13:46:33 -0500

    Maybe I'll use this code, maybe not

--- assets/js/src/view/panel-components.js
+++ assets/js/src/view/panel-components.js
@@ -52,6 +52,28 @@ class PanelSelectComponent {
 
 }
 
+// class PanelSelectDateComponent {
+
+//     constructor(options, selectType) {
+
+//         this.el = el('select.panel-card__select', { dataset: { selectType: selectType }}, 
+        
+//             new PanelOptionComponent(options)
+                
+//         );
+
+//     }
+
+// }
+
+// class PanelFetchDataComponent {
+
+//     constructor() {
+//         this.el = el('b')
+//     }
+
+// }
+
 export class PanelGAPIForm {
 
     constructor() {
@@ -66,6 +88,8 @@ export class PanelGAPIForm {
 
     }
 
+    
+
 }
 
 class PanelCard {
@@ -75,8 +99,16 @@ class PanelCard {
         this.el = el('panel-card.uk-card.uk-card-body.uk-card-primary',
 
             this.PanelCardTitle = el('h3.panel-card__title.uk-card-title', title),
-            this.panelChartArea = el('canvas.panel-chart'),
-            this.PanelGAPIForm = new PanelGAPIForm(),
+            this.panelChartArea = el('canvas#panel-chart'),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
+            new PanelGAPIForm(),
             this.PanelCardContent = el('p.panel-card__content', content)
 
         );
@@ -91,10 +123,7 @@ export class Panel {
 
         this.el = el('panel.panel.uk-grid',
 
-            this.panelCard_01 = new PanelCard("Card Title 01", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium"),
-            // this.panelCard_02 = new PanelCard("Card Title 02", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque"),
-            // this.panelCard_03 = new PanelCard("Card Title 03", "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur"),
-            // this.panelCard_04 = new PanelCard("Card Title 04", "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus")
+            this.panelCard_01 = new PanelCard("Card Title 01", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium")
 
         );
 
