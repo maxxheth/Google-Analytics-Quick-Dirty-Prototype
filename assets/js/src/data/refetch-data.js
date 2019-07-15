0538e3f0f5ecea18878a2b9ad029dc30c5625afa
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-15 00:39:44 -0500

    Added useful commentary

--- assets/js/src/data/refetch-data.js
+++ assets/js/src/data/refetch-data.js
@@ -10,6 +10,29 @@ import { setState } from './state';
 
 const googleAPIPath = '../../../../fetchgapidata.php';
 
+/**
+*
+*	This function takes the following parameters:
+*
+*	1. refetchProps: Object
+*
+*
+*	This contains two properties: newMetricArgsObjects and newDateArgsObjects
+*	Since these have already been formatted for consumption by the Google Analytics Reporting API,
+*	they are assigned as specially named properties in an object that's converted into JSON prior
+*	to being fired off to the PHP script that interfaces with the Google Analytics Reporting API.
+*
+*
+*	2. state: Object
+*
+*	This passes the current state downstream from the fetchData function as soon as
+* 	this function is called within fetchData's scope. It contains the DOM reference to the 
+*	display chart element that's used to render a dataset and update existing datasets in turn 
+*	as soon as it's fetched from the API.
+*
+*
+*/
+
 export const refetchData = refetchProps => state => {
 
     let displayChart = state[state.length - 1].displayChart;
@@ -32,8 +55,6 @@ export const refetchData = refetchProps => state => {
 
         .filter((dateArg, dateArgIndex, dateArgArray) => dateArgIndex < dateArgArray.length - 1);
 
-    console.log(JSONPackage);
-
     const xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = () => {
@@ -52,7 +73,7 @@ export const refetchData = refetchProps => state => {
 
             console.log(xhr.response);
 
-            if (isJSON(xhr.response)) {
+            if (xhr.status === 200 && isJSON(xhr.response)) {
 
                 const response = JSON.parse(xhr.response);
 
