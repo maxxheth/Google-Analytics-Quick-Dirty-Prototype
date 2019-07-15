f4b7aaa9a8b07a7bf96e2760ca0245b2c7abe182
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-15 00:40:14 -0500

    Added useful commentary

--- assets/js/src/data/state.js
+++ assets/js/src/data/state.js
@@ -1,5 +1,14 @@
 import produce from 'immer';
 
+/**
+*
+* This allows for very basic state management via immer.js, 
+* which is useful for updating the data chart based off of the 
+* latest Chart.js instance.
+*
+*
+*/
+
 export const state = {prop: ['value']};
 
 export const setState = state => newProps => {
