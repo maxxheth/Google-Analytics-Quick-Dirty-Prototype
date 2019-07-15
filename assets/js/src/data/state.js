2b72176a778428403f0a50a598cbeaba378b78c8
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-14 22:52:59 -0500

    Added an if stmt to verify that newState is not undefined

--- assets/js/src/data/state.js
+++ assets/js/src/data/state.js
@@ -2,8 +2,16 @@ import produce from 'immer';
 
 export const state = {prop: ['value']};
 
-export const setState = state => newProps => { 
+export const setState = state => newProps => {
     
-    return produce(state, newState => { newState.push(newProps); }); 
+    return produce(state, newState => { 
+
+        if (newState !== undefined) {
+
+            newState.push(newProps); 
+
+        }
+
+    });  
 
 };
