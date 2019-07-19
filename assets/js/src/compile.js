2b6f2202525fd8806ce16313056b2db42ecf5e9b
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-18 23:50:53 -0500

    I added a chart modal so that users can blow up the chart to see a larger depiction than what's displayed in the default chart view.

--- assets/js/src/compile.js
+++ assets/js/src/compile.js
@@ -7,7 +7,7 @@ import { menuEventHandler } from './events/menu-events';
 import { sendGAPIRequests } from './events/gapi-events';
 import { preSelectOptions } from './logic/pre-select-options';
 import { makeDummyChart } from './data/dummy-chart';
-
+import { prepChartModal } from './events/chart-modal';
 
 const panel = new Panel();
 
@@ -17,9 +17,11 @@ toggleButton.update('Open', true);
 
 const view = el('article.view', [toggleButton, panelTitle, panel]);
 
+const chartModal = el('aside.chart-modal__cont', el('canvas.chart-modal__chart'));
+
 const sidebar = new Menu();
 
-const dashboard = el('main.dashboard', [sidebar, view]);
+const dashboard = el('main.dashboard', [sidebar, view, chartModal]);
 
 mount(document.body, dashboard);
 
@@ -29,8 +31,12 @@ preSelectOptions();
 
 // Make dummy chart
 
-const canvasElem = document.getElementById('panel-chart');
+const canvasElem = document.querySelector('.panel-chart');
 
 makeDummyChart(canvasElem)('line');
 
 sendGAPIRequests();
+
+const chartToggleButton = document.getElementById('chart-modal-button');
+
+prepChartModal(chartModal)(chartToggleButton);
