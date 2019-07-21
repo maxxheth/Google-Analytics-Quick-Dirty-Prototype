d628c84533da73df8715fd848b67d5c31e7debcf
Maximillian Heth <maximillian.heth@gmail.com>
2019-07-21 15:08:39 -0500

    Set up a setInterval to clear the fileWatcher logs every 5 minutes.

---
+++ fileWatcher.js
@@ -0,0 +1,91 @@
+/* global require, setInterval */
+
+const gaze = require('gaze');
+
+const exec = require('child_process').exec;
+
+gaze('./assets/js/src/*.js', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', async filename => { 
+        
+        await exec('npm run bundleJS'); 
+    
+        console.log(`${filename} was changed`);
+
+    });
+
+});
+
+gaze('./assets/js/src/**/*.js', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', async filename => { 
+        
+        await exec('npm run bundleJS'); 
+    
+        console.log(`${filename} was changed`);
+
+    });
+
+});
+
+gaze('./assets/js/dist/*.js', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', filename => { 
+        
+        console.log(`${filename} was updated!`);
+    
+    });
+
+});
+
+gaze('./assets/css/src/*.scss', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', filename => {
+
+        exec('npm run bundleCSS');
+
+        console.log(`${filename} was updated!`);
+
+    });
+
+});
+
+gaze('./assets/css/src/**/*.scss', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', filename => {
+
+        exec('npm run bundleCSS');
+
+        console.log(`${filename} was updated!`);
+
+    });
+
+});
+
+gaze('./assets/css/dist/*.css', (err, watcher) => {
+
+    if (err) console.log(err);
+
+    watcher.on('changed', filename => {
+
+        console.log(`${filename} was updated!`);
+
+    });
+
+});
+
+setInterval(() => {
+
+    console.clear();
+
+}, 60 * 5000);
